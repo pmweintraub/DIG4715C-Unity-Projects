4b7a1bfccfbbd61ad632163cfbed4d5f8a7683c5
pmweintraub <pmweintraub@knights.ucf.edu>
2020-01-19 21:58:02 -0500

    10 Second Party Game

---
+++ Pick Me Up Before You Go-Go!/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;
+    public AudioSource mSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public AudioClip musicClipThree;
+    public AudioClip musicClipSix;
+
+    public Text restartText;
+    public Text helpText;
+
+    private static int playerScore;
+
+    private bool gameOver;
+    private bool restart;
+
+    private bool gamePaused;
+
+    private static float playerTime;
+
+    void Start()
+    {
+        StartCoroutine(ATime(2.0f));
+        mSource.clip = musicClipSix;
+        mSource.Play();
+        restart = false;
+        helpText.text = "Collect objects in order! Use WASD or Arrow Keys to move.";
+        gamePaused = true;
+        restartText.text = "";
+        playerTime = 10;
+    }
+
+    IEnumerator ATime(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        helpText.text = "Get the Floppy Disk!";
+        mSource.clip = musicClipOne;
+        mSource.Play();
+        gamePaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gamePaused == false)
+        {
+            playerTime -= Time.deltaTime;
+        }
+        playerScore = PlayerScript.score;
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+        if (playerScore >= 1)
+        {
+            helpText.text = "Get the Walkman!";
+        }
+
+        if (playerScore >= 2)
+        {
+            helpText.text = "Get the Camera!";
+        }
+
+        if (playerScore >= 3)
+        {
+            helpText.text = "Get the Cell Phone!";
+        }
+
+        if (playerScore >= 4)
+        {
+            if (mSource.clip == musicClipOne)
+            {
+
+                mSource.clip = musicClipTwo;
+
+                mSource.Play();
+
+            }
+            helpText.text = "";
+            gameOver = true;
+            restart = true;
+        }
+
+        if (playerTime < 0)
+        {
+            if (mSource.clip == musicClipOne)
+            {
+                mSource.clip = musicClipThree;
+
+                mSource.Play();
+            }
+            helpText.text = "";
+            gameOver = true;
+            restart = true;
+        }
+
+        if (gameOver)
+        {
+            //StartCoroutine(BTime(2.0f));
+            restartText.text = "Press 'R' To Restart";
+            restart = true;
+
+        }
+
+        /*IEnumerator BTime(float waitTime)
+        {
+            yield return new WaitForSeconds(waitTime);
+            SceneManager.LoadScene("Main");
+        }*/
+
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("Main");
+            }
+        }
+    }
+
+}
---
+++ Pick Me Up Before You Go-Go!/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioClip musicClipFour;
+    public AudioClip musicClipFive;
+    public float speed;
+    public Text winText;
+    public Text scoreText;
+    public Text timeText;
+
+    public GameObject player;
+    public GameObject sparkle;
+    public GameObject box;
+    public GameObject loser;
+
+
+    public static float timeLeft;
+    private Rigidbody2D rd2d;
+    private bool facingRight = true;
+    public static int score;
+    Animator anim;
+
+    private bool gamePaused;
+
+
+    void Start()
+    {
+        StartCoroutine(ATime(2.0f));
+        rd2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        winText.text = "";
+        gamePaused = true;
+        timeLeft = 10;
+        score = 0;
+        SetScoreText();
+    }
+
+    IEnumerator ATime(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        box.SetActive(false);
+        gamePaused = false;
+    }
+
+    void Update()
+    {
+        SetTimeText();
+        InOrder();
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("State", 0);
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
+    void FixedUpdate()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(hozMovement, vertMovement).normalized;
+        rd2d.velocity = movement * speed;
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            score = score + 1;
+            SetScoreText();
+            musicSource.clip = musicClipFour;
+            musicSource.Play();
+        }
+    }
+
+    void InOrder()
+    {
+        if(score <= 0)
+        {
+            Physics2D.IgnoreLayerCollision(8, 10, true);
+            Physics2D.IgnoreLayerCollision(8, 11, true);
+            Physics2D.IgnoreLayerCollision(8, 12, true);
+        }
+
+        if (score >= 1)
+        {
+            Physics2D.IgnoreLayerCollision(8, 10, false);
+        }
+
+        if (score >=2)
+        {
+            Physics2D.IgnoreLayerCollision(8, 11, false);
+        }
+
+        if (score >= 3)
+        {
+            Physics2D.IgnoreLayerCollision(8, 12, false);
+        }
+
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Floor")
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                musicSource.clip = musicClipFive;
+                musicSource.Play();
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                musicSource.clip = musicClipFive;
+                musicSource.Play();
+            }
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Chao-Jump"))
+            {
+                anim.SetInteger("State", 0);
+            }
+        }
+    }
+
+
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        if (score >= 4)
+        {
+            winText.text = "You win! Game created by Paulina Weintraub!";
+            sparkle.SetActive(true);
+
+        }
+    }
+
+    void SetTimeText()
+    {
+        if (score != 4)
+        {
+            if (gamePaused == false)
+            {
+                timeLeft -= Time.deltaTime;
+            }
+            timeText.text = "Time: " + timeLeft.ToString("F0");
+            if (timeLeft < 0)
+            {
+                Destroy(player);
+                loser.SetActive(true);
+                winText.text = "You lose! Game created by Paulina Weintraub!";
+            }
+        }
+    }
+
+}
